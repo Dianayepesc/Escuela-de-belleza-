f646df7d216deb74dad7e1769b2fd8b6af10086f
Dianayepesc <114169740+Dianayepesc@users.noreply.github.com>
2022-09-29 18:15:36 -0500

    Update ProyectoBelleza.py

--- ProyectoBelleza.py
+++ ProyectoBelleza.py
@@ -3,13 +3,32 @@ print("1: Información de la empresa")
 print("2: Comprar")
 print("3: Catálogo")
 print("4: Agendar Cita")
+print("5: Cancelacion de cita")
 x=int(input("Digíte una opción: "))
 if x==2:
       print("¿Está registrado?")
       print("1: Si")
       print("2: No")
       y=int(input("Digíte una opción: "))
-      if y==1:
-          q=int(input("Digíte su identificación: "))
-          print("Gracias por su compra")
+elif x==1:
+   q=int(input("Digíte su identificación: "))
+   print("Gracias por su compra")
+elif x==3:
+      b=str(input("Que producto desea observar"))
+      print("Producto que desea)
+      print("productos relacionados a su eleccion")
+            else:
+            print("su producto no fue encontrado")
+elif x==4:
+   print("desea agendar una cita con nosotros?")
+   print("digite 1 para si,2 para no")
+   w=int(input("digite opcion"))
+         if w==1
+         print("Desea que su compra sea a domicilio 1 para si,2 para no")
+         z=int(input("digite su opcion))
+                if z==1:
+                  e=str(input("digite direccion"))
+                else z==2:
+                  print("Acerquese a nuestros puntos oficiales que mas se acerquen a su necesidad)
+       
           
