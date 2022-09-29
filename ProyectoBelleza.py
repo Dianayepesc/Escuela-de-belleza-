505ad7fa1295b49f3cdc8b6eca40cc1ea4be473e
María Fernanda Vélez <114169327+mwedderburn@users.noreply.github.com>
2022-09-29 17:55:04 -0500

    Update ProyectoBelleza.py

--- ProyectoBelleza.py
+++ ProyectoBelleza.py
@@ -1,4 +1,15 @@
 print("Menú")
-print("1: Compras")
-print("2: Catálogo")
-print("3: Agendar Cita")
+print("1: Información de la empresa")
+print("2: Comprar")
+print("3: Catálogo")
+print("4: Agendar Cita")
+x=int(input("Digíte una opción: "))
+if x==2:
+      print("¿Está registrado?")
+      print("1: Si")
+      print("2: No")
+      y=int(input("Digíte una opción: "))
+      if y==1:
+          q=int(input("Digíte su identificación: "))
+          print("Gracias por su compra")
+          
