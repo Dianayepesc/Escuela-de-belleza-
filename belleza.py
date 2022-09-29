703629aab8008082e166fb5ae4978e382768816b
María Fernanda Vélez <114169327+mwedderburn@users.noreply.github.com>
2022-09-29 17:53:56 -0500

    Update belleza.py

--- belleza.py
+++ belleza.py
@@ -1,5 +1,14 @@
 print("Menú")
-print("1: Compras")
+print("1: Comprar")
 print("2: Catálogo")
 print("3: Agendar Cita")
-
+x=int(input("Digíte una opción: "))
+if x==1:
+      print("¿Está registrado?")
+      print("1: Si")
+      print("2: No")
+      y=int(input("Digíte una opción: "))
+      if y==1:
+          q=int(input("Digíte su identificación: "))
+          print("Gracias por su compra")
+          
