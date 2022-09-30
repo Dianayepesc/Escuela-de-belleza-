df75e562af6d76c034aedd5a47a140dab08896a8
Dianayepesc <114169740+Dianayepesc@users.noreply.github.com>
2022-09-29 19:30:48 -0500

    Update Diana.py

--- Diana.py
+++ Diana.py
@@ -1,3 +1,14 @@
-opcion=int (input("digite opcion"))
-if (opcion==2):
-  
+elif x==3:
+    v=float(input("¿A qué hora desearía agendar una cita con nosotros?"))
+    print("Su cita se ha agendado exitósamente")
+elif x==4: 
+    print("Compras a domicilio")
+    w=int(input("Digite opcion: "))
+    if w==1:
+        print("Desea que su compra sea a domicilio 1 para si, 2 para no ")
+        z=int(input("Digite su opcion: "))
+        if z==1:
+            e=str(input("Digite direccion: "))
+        elif z==2:
+            print("Acerquese a nuestros puntos oficiales que mas se acerquen a su necesidad")
+ 
